fde6aff03cc9fe6a95cbc15c50cc9158fd475935
Gabby <gabzo24@live.com.au>
2024-01-06 17:06:34 +1100

    controllers

---
+++ controllers/developerSkillsController.js
@@ -0,0 +1,17 @@
+const developerSkills = require('../models/developerSkills');
+
+function getAllSkills(req, res) {
+    const allSkills = developerSkillsModel.getAllSkills();
+    res.render('skills/index', { skills: allSkills });
+  };
+  
+  function getSkillById(req, res) {
+    const skillId = parseInt(req.params.id);
+    const skill = developerSkillsModel.getSkillById(skillId);
+    res.render('skills/show', { skill });
+  };
+  
+  module.exports = {
+    getAllSkills,
+    getSkillById,
+  };
